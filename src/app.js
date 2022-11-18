d48c7399d3db5516070ed843f559f4a998db93be
dmateosj <76000914+dmateosj@users.noreply.github.com>
2022-11-18 11:12:00 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,9 +15,10 @@ app.use("/", indexRoutes);
 app.use("/api", employeesRoutes);
 
 app.use((req, res, next) => {res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
