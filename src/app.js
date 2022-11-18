b0fe0f2e8e82a1a7528bfb259efeab371bc00fdd
dmateosj <76000914+dmateosj@users.noreply.github.com>
2022-11-18 11:37:49 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,8 +14,10 @@ app.use(express.json());
 app.use("/", indexRoutes);
 app.use("/api", employeesRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Not found" });
+app.use((req, res, next) => {res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 });
 
 export default app;
